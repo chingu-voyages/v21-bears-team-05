50db3164c829838094c1db698c57ef4d4de193ed
Lily <lily@pop-os.localdomain>
2020-07-07 10:57:36 +0100

    Edit Recipe to make static graphQL query

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 import './Recipe.css'
 
-const Recipe = ({pretendRecipeData = {colour: "black", title: "", ingredients: ""}}) => {
+export const GET_RECIPE = gql`
+  query GetRecipe {
+    pretendRecipe {
+      colour
+      title
+      ingredients
+    }
+  }
+`
+
+const Recipe = () => {
+  const { loading, error, data } = useQuery(GET_RECIPE)
+  if (loading) return "Loading..."
+  if (error) return `Error! ${error.message}`
   return (
-    <div style={{background: pretendRecipeData.colour}}>
-        <h1>{pretendRecipeData.title}</h1>
-        <p>With {pretendRecipeData.ingredients}</p>
+    <div style={{background: data.pretendRecipe.colour}}>
+        <h1>{data.pretendRecipe.title}</h1>
+        <p>With {data.pretendRecipe.ingredients}</p>
     </div>
   )
 }
