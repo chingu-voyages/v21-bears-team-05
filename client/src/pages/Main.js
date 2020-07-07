e5175f2da0a8e2fb8629aaff7ce86a358ad2c563
Lily <lily@pop-os.localdomain>
2020-07-07 10:58:07 +0100

    Remove old rest data fetch from Main

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,48 +1,18 @@
 import React, {useState, useRef, useEffect} from 'react'
-import axios from 'axios'
 import Layout from '../components/Layout'
 import Recipe from '../components/Recipe'
 import './Main.css'
 
 const Main = () => {
-  const fetchedRecipes = useRef([])
-  const [activeRecipeIndex, setActiveRecipeIndex] = useState(0)
-  const [pretendRecipeData, setPretendRecipeData] = useState({})
-  const getRecipe = async () => {
-    const recipe = await axios.get('/api/pretendRecipe')
-    return recipe.data
-  }
-  const handlePrev = async () => {
-    if (activeRecipeIndex > 0) {
-        setActiveRecipeIndex(activeRecipeIndex - 1)
-        setPretendRecipeData(fetchedRecipes.current[activeRecipeIndex - 1])
-    }
-    return
-  }
-  const handleNext = async () => {
-    if (fetchedRecipes.current.length - 1 <= activeRecipeIndex) {
-        const recipe = await getRecipe()
-        fetchedRecipes.current.push(recipe)
-    } 
-    setActiveRecipeIndex(activeRecipeIndex + 1)
-    setPretendRecipeData(fetchedRecipes.current[activeRecipeIndex + 1])
-    return
-  }
-  useEffect(() => {
-    getRecipe().then(recipe => {
-        fetchedRecipes.current.push(recipe)
-        setPretendRecipeData(fetchedRecipes.current[activeRecipeIndex])
-    })
-  }, [])
   return (
     <Layout>
         <section>
-            <Recipe {...{pretendRecipeData}} />
+            <Recipe />
         </section>
         <section>
-            <button onClick={handlePrev}>Prev</button>
+            <button>Prev</button>
             <div>TODO Filters</div>
-            <button onClick={handleNext}>Next</button>
+            <button>Next</button>
         </section>
     </Layout>
   )
