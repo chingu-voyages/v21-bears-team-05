9c81d8f7c59fff6da4b4fadb4c2c50f5fe21a5a5
Lily <lily@pop-os.localdomain>
2020-07-08 21:26:26 +0100

    Impliment IngredientsSearch in Cupboard

--- client/src/pages/Cupboard.js
+++ client/src/pages/Cupboard.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
+import IngredientsSearch from '../components/IngredientSearch'
+import ItemsList from '../components/ItemsList'
 import './Cupboard.css'
 
+
 const Cupboard = () => {
+  const [ingredientsList, setIngredientsList] = useState([])
+  const handleRemoveIngredient = obj => {
+    setIngredientsList(ingredientsList.filter(ingredient => JSON.stringify(ingredient) !== JSON.stringify(obj)))
+  }
+  const addToIngredientsList = item => {
+    setIngredientsList([...new Set([...ingredientsList, item])])
+  }
   return (
     <Layout>
-        <div className="Cupboard">
-            Cupboard TODO
+        <div className="cupboard">
+          <div className="cupboard__list"> 
+            <ItemsList list={ingredientsList.map(item => ({...item, removeSelf: () => handleRemoveIngredient(item)}))} type="cupboard-item" />
+          </div>
+          <IngredientsSearch {...{addToIngredientsList}} />
         </div>
     </Layout>
   )
