26a60d73443a622d283c91b50e61b4e91bb5903e
Lily <lily@pop-os.localdomain>
2020-07-08 21:26:45 +0100

    Remove no longer used CatagoryItem

--- client/src/components/CatagoryItem.js
+++
@@ -1,8 +0,0 @@
-const CatagoryItem = ({title}) => (
-    <div className="results__catagory">
-        {/* TODO dynmaically import image/icon */}
-        {title}
-    </div>
-)
-
-export default CatagoryItem
