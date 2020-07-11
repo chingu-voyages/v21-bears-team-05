047ba06196fc70c2670e8ce81e75a894b34183b2
Laptop <pierrolabo@gmail.com>
2020-07-11 14:23:42 +0200

    Send facebook accessToken to backend

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { AuthContext } from '../App';
 import logo from '../logo.svg';
 import './Landing.css';
 import { Redirect } from 'react-router-dom';
@@ -6,12 +7,38 @@ import { Redirect } from 'react-router-dom';
 import FacebookLogin from 'react-facebook-login';
 
 const Landing = () => {
+  const initialState = {
+    isSubmitting: false,
+    errorMessage: null,
+  };
   const [redirect, setRedirect] = useState(null);
+  const [data, setData] = React.useState(initialState);
+  const { dispatch } = React.useContext(AuthContext);
   const signIn = () => {
     setRedirect(<Redirect to='/main/' />);
   };
+  //  Token and data received from Facebook OAUTH
   const responseFacebook = (res) => {
+    const { accessToken } = res;
     console.log('responseFacebook', res);
+    console.log('accessToken', accessToken);
+    setData({
+      ...data,
+      isSubmitting: true,
+      errorMessage: null,
+    });
+
+    //  Send the access token to our backend
+    fetch('http://localhost:5000/auth/oauth/facebook', {
+      method: 'post',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ accessToken }),
+    }).then((res) => {
+      console.log('fetch: ', res);
+    });
   };
   return (
     <div className='landing'>
