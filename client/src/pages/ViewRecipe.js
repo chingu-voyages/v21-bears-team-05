315a4fe39e2d95b02e09fb05121be16e6db46df2
marcoruggeri <4high20@gmail.com>
2020-07-12 17:20:32 +0100

    Add back button in recipe page

--- client/src/pages/ViewRecipe.js
+++ client/src/pages/ViewRecipe.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 
 const ViewRecipe = (props) => {
   const { recipe } = props.location.state;
-  console.log(recipe);
   return (
     <Layout>
+      <Link to="/recipes">Go back</Link>
       <h1>{recipe.name}</h1>
       <p>Ingredients:</p>
       {recipe.ingredients.map((ingredient) => (
