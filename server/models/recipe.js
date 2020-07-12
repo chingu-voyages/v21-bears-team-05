5015fe352ade04e06ba23669f32208c0b09693e1
jidemobell <jidd32@gmail.com>
2020-07-12 04:01:59 +0100

    add a recipe model

---
+++ server/models/recipe.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const ingredientSchema = new Schema({
+	name: { type: String, required: true, lowercase: true }
+})
+
+const recipeSchema = new Schema({
+	name: { type: String, required: true, lowercase: true },
+	ingredients: [ingredientSchema],
+	description: String,
+	resgion: String,
+	created_by: { type: Schema.Types.ObjectId, required: true },
+	how_to: { type: String },
+	meta: {
+		votes: Number,
+		favs:  Number
+	}
+},
+{
+	timestamps: {createdAt: 'date_created', updatedAt: 'date_updated'}
+})
+// a user will have a wish list of recipes
+
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
