e79677098642560c4c9cf46919a700bdd8df64bd
jidemobell <jidd32@gmail.com>
2020-07-14 02:35:18 +0100

    format controller codes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../controllers/user')
+const express = require("express");
+const router = express.Router();
+const User = require("../controllers/user");
 
-router.get('/:id', (req, res) => {
-	//  const user = req.user;
-  const { id } = req.params
-  User.findUserById(id, res)
-})
+router.get("/:id", (req, res) => {
+  //  const user = req.user;
+  const { id } = req.params;
+  User.findUserById(id, res);
+});
 
-module.exports = router
+module.exports = router;
