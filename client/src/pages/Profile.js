96e03e3a31fb1f02b475fd3639ce3585b593d311
Jack Randol <jackrandol@gmail.com>
2020-07-20 11:10:13 +0200

    removed bio / added the editor into profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,8 +9,6 @@ const Profile = () => {
     username: "Susan",
     avatar:
       "https://d33wubrfki0l68.cloudfront.net/ec99b69e4512106ddf49a54a31a92853b19b2c6a/28b45/en/blog/uploads/web-developer-coding-backend.jpg",
-    bio:
-      "I love my job as a coder but I forget to plan my meals and only have 2 eggs, 1 potato and some old mustard.  I hope this app will help me figure out what to have for dinner.",
   };
 
   const [editorVisible, setEditorVisible] = useState(false);
@@ -40,18 +38,36 @@ const Profile = () => {
                 src={userData.avatar}
                 alt="profile pic"
               ></img>
-              <p>{userData.bio}</p>
             </div>
           )}
           <button onClick={toggleProfileEditor}>edit profile</button>
         </div>
       )}
       {editorVisible && (
-        <ProfileEditor
-          userData={userData}
-          toggle={toggleProfileEditor}
-          handleChange={handleChange}
-        />
+        <div>
+          <h1>Edit Profile</h1>
+          <form onSubmit={toggleProfileEditor}>
+            <label>
+              username:
+              <input
+                type="text"
+                name="username"
+                value={userData.username}
+                onChange={handleChange}
+              ></input>
+            </label>
+            <label>
+              avatar:
+              <input
+                type="text"
+                name="avatar"
+                value={userData.avatar}
+                onChange={handleChange}
+              ></input>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       )}
       <Link to="/publishrecipe">publish recipe</Link>
     </Layout>
