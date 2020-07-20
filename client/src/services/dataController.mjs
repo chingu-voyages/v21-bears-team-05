a9010ace208118b3b7533d55b27284051b40d4ce
lily-law <lilylaw.dev@gmail.com>
2020-07-20 16:21:19 +0100

    Start refactoring dataController for new design

--- client/src/services/dataController.mjs
+++ client/src/services/dataController.mjs
@@ -1,12 +1,78 @@
 import localDB from './localDB'
+import serverAPI from './localDB'
+// import genTempId from '../utils/genTempId' // TODO
 
 const config = {
     pageLength: 25
 }
 
-const isOnline = async () => {
-    // TODO check if able to connect
+const data = {
+    recipes,
+    ingredients,
+    users,
+    index,
+    queue
+}
+
+const addData = async ({into, newData}) => {
+    const destinationIsValid = checkDestinationIsValid(into)
+    if (!destinationIsValid) {
+        return destinationIsValid
+    }
+    data[into] = {...data[into], ...newData}
+    addToQueue({destination: into, data: newData})
+    await localDB.write({into, data: newData})
+    runQueue()
+    return true
+}
+
+const editData = async ({into, ref, newData}) => {
+    const refExists = checkRefExists(ref, into)
+    if (!refExists) {
+        return refExists
+    }
+    data[into][ref] = {...data[into][ref], ...newData}
+    addToQueue({destination: into, data: newData})
+    await localDB.write({into, data: newData})
+    runQueue()
+    return true
+}
+
+const getData = async ({from, ref, page}) => {
+    const refExistsInIndex = checkRefExists(ref, "index")
+    if (refExistsInIndex) {
+        let data = await serverAPI.isOnline() && await Promise.resolve(false) // TODO fetch from sever
+        if (data) {
+            localDB.write({into: from, data})
+        }
+        else {
+
+            data = await localDB.read({from, page, pageLength: config.pageLength})
+        }
+        return data && {data, next: !data || !Array.isArray(data) || data.length < config.pageLength ? "end" : page+1}
+    } 
     return false
+}
+
+export { addData, getData }
+
+const checkDestinationIsValid = destination => {
+    if (!data[into]) {
+        console.warn(`No destination/store for ${into}`)
+        return false
+    }
+    return true
+}
+
+const checkRefExists = (ref, destination) => {
+    const destinationIsValid = checkDestinationIsValid(destination)
+    if (!destinationIsValid) {
+        return destinationIsValid
+    }
+    if (!data[destination].hasOwnProperty(ref)) {
+        console.warn(`No such data in ${from} with ref ${ref}`)
+        return false
+    }
 }
 
 const addToQueue = ({destination, data}) => {
@@ -14,7 +80,7 @@ const addToQueue = ({destination, data}) => {
 }
 
 const runQueue = async () => { 
-    if (await isOnline()) {
+    if (await serverAPI.isOnline()) {
         const queue = await localDB.read("queue")
         while (queue.length > 0) {
             const nextOperation = queue.shift()
@@ -29,24 +95,17 @@ const runQueue = async () => {
     }
 }
 runQueue()
- 
-const addData = async ({into, data}) => {
-    addToQueue({destination: into, data})
-    await localDB.write({into, data})
-    runQueue()
-    return true
+
+const init = () => {
+    // load from storage
+    data.recipes = await localDB.read("recipes") || {}
+    data.ingredients = await localDB.read("ingredients") || {}
+    data.users = await localDB.read("users") || {}
+    data.index = await localDB.read("index") || {}
+    data.queue = await localDB.read("queue") || []
+    // sync from server
+    // syncIndex
+    // getUser
+    // runQueue
 }
-
-const getData = async ({from, ref, page}) => {
-    let data = await isOnline() && await Promise.resolve(false) // TODO fetch from sever
-    if (data) {
-        localDB.write({into: from, data})
-    }
-    else {
-        data = await localDB.read({from, ref, page, pageLength: config.pageLength})
-    }
-    return data && {data, next: !data || !Array.isArray(data) || data.length < config.pageLength ? "end" : page+1}
-}
-
-
-export { addData, getData }
+init()
