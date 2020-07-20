1e5697f2a5379fedf07150af76739d89d01a7285
lily-law <lilylaw.dev@gmail.com>
2020-07-20 16:35:06 +0100

    Run Prettier formatting

--- client/src/services/serverAPI.mjs
+++ client/src/services/serverAPI.mjs
@@ -1,22 +1,28 @@
 const isOnline = async () => {
-    // TODO ping server
-    console.log(`TODO: isOnline method`)
-}
-const getData = async ({destination, ref}) => {
-    // TODO
-    console.log(`TODO: getData method; destination: ${destination}, ref: ${ref}`)
-}
-const postData = async ({destination, data}) => {
-    // TODO
-    console.log(`TODO: postData method; destination: ${destination}, data: ${data}`)
-}
-const putData = async ({destination, ref, data}) => {
-    // TODO
-    console.log(`TODO: putData method; destination: ${destination}, ref: ${ref}, data: ${data}`)
-}
-const deleteData = async ({destination, ref}) => {
-    // TODO
-    console.log(`TODO: deleteData method; destination: ${destination}, ref: ${ref}`)
-}
+  // TODO ping server
+  console.log(`TODO: isOnline method`);
+};
+const getData = async ({ destination, ref }) => {
+  // TODO
+  console.log(`TODO: getData method; destination: ${destination}, ref: ${ref}`);
+};
+const postData = async ({ destination, data }) => {
+  // TODO
+  console.log(
+    `TODO: postData method; destination: ${destination}, data: ${data}`
+  );
+};
+const putData = async ({ destination, ref, data }) => {
+  // TODO
+  console.log(
+    `TODO: putData method; destination: ${destination}, ref: ${ref}, data: ${data}`
+  );
+};
+const deleteData = async ({ destination, ref }) => {
+  // TODO
+  console.log(
+    `TODO: deleteData method; destination: ${destination}, ref: ${ref}`
+  );
+};
 
-export default {isOnline, getData, postData, putData, deleteData}
+export default { isOnline, getData, postData, putData, deleteData };
