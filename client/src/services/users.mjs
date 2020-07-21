64781919e05d4501f60f71021aed7c4cf9d82ef2
lily-law <lilylaw.dev@gmail.com>
2020-07-21 20:07:21 +0100

    Update user for new data design

--- client/src/services/users.mjs
+++ client/src/services/users.mjs
@@ -1,24 +1,35 @@
 import { addData, getData } from "./dataController.mjs";
 
-const userID = 0; // TODO get from auth
+const userID = 'currentUserId'; // TODO get from auth
 
 const updateUserData = async ({ data }) => {
   const currentUserData = await getUserData();
-  await addData({ into: "user", data: { ...currentUserData, ...data } });
+  await addData({ into: "users", data: { ...currentUserData, ...data } });
   return true;
 };
 
-const getUserData = async () => {
-  let user = await getData({ from: "user", ref: { userID } });
-  if (!user.data[0]) {
-    await newUser({ userID: 0 });
-    user = await getData({ from: "user", ref: { userID } });
+const getUserData = async ({ ref } = { ref: null }) => {
+  let userData;
+  if (ref) {
+    userData = getData({from: "users", ref})
   }
-  return user.data[0];
+  else {
+    if (!userID) {
+      console.error(`User not authorised`)
+    }
+    else {
+      userData = getData({ from: "users", userID })
+      if (!userData) {
+        await newUser()
+        userData = getData({ from: "users", userID })
+      }
+    } 
+  }
+  return userData
 };
 
-const updateCupboard = async (cupboard) => {
-  await updateUserData({ data: { cupboard } });
+const updateCupboard = async ({ ingredients }) => {
+  await updateUserData({ data: { cupboard: ingredients } });
   return true;
 };
 
@@ -27,8 +38,8 @@ const getCupboard = async () => {
   return data.cupboard || [];
 };
 
-const newUser = async ({ userID }) => {
-  await addData({ into: "user", data: { userID } });
+const newUser = async () => {
+  await addData({ into: "users", data: { id: userID } });
   return true;
 };
 
