f6507b4a314d4b87fdb2e65d617308cddbf63971
Laptop <pierrolabo@gmail.com>
2020-07-24 00:37:58 +0200

    POST addUserData PassportJWT verification

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const router = express.Router();
+const router = require("express-promise-router")();
+
+const passport = require("passport");
+const passportConf = require("../services/passport");
+const passeportJWT = passport.authenticate("jwt", { session: false });
+
+const { validateBody, schemas } = require("../helpers/routeHelpers");
+const bodyValidation = validateBody(schemas.authSchema);
 const UserController = require("../controllers/user");
 
 //  Route for getting User Data
@@ -13,5 +20,5 @@ router.route("/:id").get(UserController.findUserById);
 //  @route POST /users/:id
 //  @desc   Add data to User
 //  @access Public
-router.route("/:id").post(UserController.addUserData);
+router.route("/:id").post(passeportJWT, UserController.addUserData);
 module.exports = router;
