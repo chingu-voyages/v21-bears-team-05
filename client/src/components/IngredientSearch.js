f892fcd815a9a80e67ecbd2d5e74019b93134079
lily-law <lilylaw.dev@gmail.com>
2020-07-23 12:56:09 +0100

    Refactor ingredients to new data design

--- client/src/components/IngredientSearch.js
+++ client/src/components/IngredientSearch.js
@@ -1,92 +1,105 @@
-import React, { useRef, useState, useEffect, useCallback } from 'react'
-import { searchIngredients, getCatagories } from '../services/ingredients.mjs'
-import ItemsList from './ItemsList'
-import ListItem from './ListItem'
-import './IngredientSearch.css'
+import React, { useRef, useState, useCallback, useEffect } from "react";
+import { searchIngredients, getCatagories } from "../services/ingredients.mjs";
+import ItemsList from "./ItemsList";
+import ListItem from "./ListItem";
+import "./IngredientSearch.css";
 
-const IngredientSearch = ({addToIngredientsList}) => {
-    const [query, setQuery] = useState("")
-    const [breadcrumbs, setBreadcrumbs] = useState([])
-    const [catagories, setCatagories] = useState(getCatagories())
-    const [results, setResults] = useState([])
-    const inputRef = useRef()
-    const handleQuery = useCallback( () => {
-        const value = inputRef.current.value
-        setQuery(value)
-        const matches = (value === "" && breadcrumbs.length === 0) ? [] : searchIngredients(value, breadcrumbs.map(({title}) => title));
-        setResults(matches.filter(({isCatagory}) => !isCatagory))
-        setCatagories(matches.filter(({isCatagory}) => isCatagory))
-    }, [breadcrumbs])
-    const handleKeyPress = e => {
-        if (e.key === "Backspace" && query === "" && breadcrumbs.length > 0) {
-            setBreadcrumbs(breadcrumbs.slice(0, breadcrumbs.length-1))
-        }
-        if (e.key === "Enter") {
-            inputRef.current.blur()
-        }
+const IngredientSearch = ({ addToIngredientsList }) => {
+  const [query, setQuery] = useState("");
+  const [breadcrumbs, setBreadcrumbs] = useState([]);
+  const [catagories, setCatagories] = useState([]);
+  const [results, setResults] = useState([]);
+  const inputRef = useRef();
+  const handleQuery = useCallback(() => {
+    const value = inputRef.current.value;
+    setQuery(value);
+    const matches =
+      value === "" && breadcrumbs.length === 0
+        ? []
+        : searchIngredients(value, breadcrumbs);
+    setResults(matches);
+  }, [breadcrumbs]);
+  const handleKeyPress = (e) => {
+    if (e.key === "Backspace" && query === "" && breadcrumbs.length > 0) {
+      handleRemoveBreadcrumb();
     }
-    const handleAddBreadcrumb = item => {
-        setBreadcrumbs([...breadcrumbs, item])
-        setQuery("")
-        inputRef.current.focus()
+    if (e.key === "Enter") {
+      inputRef.current.blur();
     }
-    const clearSearch = () => {
-        setBreadcrumbs([])
-        inputRef.current.value = ""
-        handleQuery()
+  };
+  const handleAddBreadcrumb = (item) => {
+    const newBreadCrumbs = [...breadcrumbs, item];
+    setBreadcrumbs(newBreadCrumbs);
+    const newCatagories = getCatagories(newBreadCrumbs);
+    setCatagories(newCatagories);
+    setQuery("");
+    inputRef.current.focus();
+  };
+  const handleRemoveBreadcrumb = () => {
+    const newBreadCrumbs = breadcrumbs.slice(0, breadcrumbs.length - 1);
+    setBreadcrumbs(newBreadCrumbs);
+    const newCatagories = getCatagories(newBreadCrumbs);
+    setCatagories(newCatagories);
+    if (breadcrumbs.length === 0 && query === "") {
+      setResults([]);
     }
-    useEffect(() => {
-        handleQuery()
-    }, [breadcrumbs, handleQuery])
-    useEffect(() => {
-        if (breadcrumbs.length === 0 && query === "") {
-            setCatagories(getCatagories())
-        }
-    }, [query, breadcrumbs])
-    return (
-        <div className="ingredient-search">
-            <div className="ingredient-search__query">
-                <ItemsList list={breadcrumbs.map(item => ({...item}))} type="breadcrumb" />
-                <input className="ingredient-search__input" onChange={handleQuery} value={query} ref={inputRef} onKeyDown={handleKeyPress} />
-                {(query || breadcrumbs.length > 0) && <button onClick={clearSearch}>X</button>}
-            </div>
-            <div className="ingredient-search__catagories">
-                {catagories.map(result => (
-                        <ListItem
-                            key={"results__catagory--"+result.title} 
-                            {...{
-                                ...result,
-                                onClick: () => handleAddBreadcrumb(result)
-                            }} 
-                            type="catagory"
-                        /> 
-                    )
-                )}
-            </div>
-            <div className="ingredient-search__results">
-                {results.map(result => (
-                    result.isCatagory ? 
-                        <ListItem
-                            key={"results__catagory--"+result.title} 
-                            {...{
-                                ...result,
-                                onClick: () => setBreadcrumbs([...breadcrumbs, result])
-                            }} 
-                            type="results__catagory"
-                        /> :
-                        <ListItem 
-                            key={"results__ingredient--"+result.title} 
-                            {...{
-                                ...result, 
-                                onClick: () => addToIngredientsList(result)
-                            }}
-                            type="results__ingredient"
-                        />
-                    )
-                )}
-            </div>
-        </div>
-    )
-}
+  };
+  const clearSearch = () => {
+    setBreadcrumbs([]);
+    setCatagories(getCatagories());
+    setResults([]);
+    setQuery("");
+  };
+  useEffect(() => {
+    handleQuery();
+  }, [handleQuery]);
+  useEffect(() => {
+    setCatagories(getCatagories());
+  }, []);
+  return (
+    <div className="ingredient-search">
+      <div className="ingredient-search__query">
+        <ItemsList
+          list={breadcrumbs.map((item) => ({ ...item }))}
+          type="breadcrumb"
+        />
+        <input
+          className="ingredient-search__input"
+          onChange={handleQuery}
+          value={query}
+          ref={inputRef}
+          onKeyDown={handleKeyPress}
+        />
+        {(query || breadcrumbs.length > 0) && (
+          <button onClick={clearSearch}>X</button>
+        )}
+      </div>
+      <div className="ingredient-search__catagories">
+        {catagories.map((result) => (
+          <ListItem
+            key={"results__catagory--" + result.title}
+            {...{
+              ...result,
+              onClick: () => handleAddBreadcrumb(result),
+            }}
+            type="catagory"
+          />
+        ))}
+      </div>
+      <div className="ingredient-search__results">
+        {results.map((result) => (
+          <ListItem
+            key={"results__ingredient--" + result.title}
+            {...{
+              ...result,
+              onClick: () => addToIngredientsList(result),
+            }}
+            type="results__ingredient"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default IngredientSearch
+export default IngredientSearch;
--- client/src/services/ingredients.mjs
+++ client/src/services/ingredients.mjs
@@ -4,95 +4,425 @@ import traversePath from "../utils/traversePath.mjs";
 const data = (function dummyData() {
   /* TODO replace with call to backend */
   return {
-    vegetables: [
-      "tomatos",
-      "cucumber",
-      "spinach",
-      "lettuce",
-      "bell pepper",
-      "radish",
-      "sprouts",
-      "cabbage",
-      "fennel",
-      "onion",
-      "garlic",
-      "olive",
-      "carrot",
-      "potato",
+    catagories: [
+      {
+        id: "catagory1",
+        name: "vegetables",
+      },
+      {
+        id: "catagory2",
+        name: "meat",
+      },
+      {
+        id: "catagory3",
+        name: "fish",
+      },
+      {
+        id: "catagory4",
+        name: "fruit",
+      },
+      {
+        id: "catagory5",
+        name: "spices",
+      },
+      {
+        id: "catagory6",
+        name: "chilies",
+        parent: "catagory5",
+      },
     ],
-    meat: ["beef", "chicken", "ham", "pork", "ground beef", "sausages"],
-    fish: ["tuna", "salmon", "talapia", "perch", "swordfish", "cod"],
-    fruit: [
-      "banana",
-      "cherry",
-      "raspberry",
-      "apple",
-      "apricot",
-      "kiwi",
-      "orange",
-      "blueberry",
-      "blackberry",
-      "grapefruit",
-      "honeydew melon",
-      "cantoulope",
-      "grapes",
-      "plum",
-    ],
-    spices: [
-      "pepper",
-      "salt",
-      "oregano",
-      "thyme",
-      "basil",
-      "nutmeg",
-      "allspice",
-      "cinnamon",
-      "ginger",
-      "bay leaf",
-      "marjoram",
-      "tumeric",
-      "sage",
-      "curry",
-      "vanilla",
-      "cumin",
-      "carraway",
-      "chili flakes",
+    ingredients: [
+      {
+        id: "ingredient1",
+        name: "tomatos",
+        catagories: ["catagory1", "catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient2",
+        name: "cucumber",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient3",
+        name: "spinach",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient4",
+        name: "lettuce",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient5",
+        name: "bell pepper",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient6",
+        name: "radish",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient7",
+        name: "sprouts",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient8",
+        name: "cabbage",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient9",
+        name: "fennel",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient10",
+        name: "onion",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient11",
+        name: "garlic",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient12",
+        name: "olive",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient13",
+        name: "carrot",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient14",
+        name: "potato",
+        catagories: ["catagory1"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient15",
+        name: "beef",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient16",
+        name: "chicken",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient17",
+        name: "ham",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient18",
+        name: "pork",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient19",
+        name: "ground beef",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient20",
+        name: "sausages",
+        catagories: ["catagory2"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient21",
+        name: "tuna",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient22",
+        name: "salmon",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient23",
+        name: "talapia",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient24",
+        name: "perch",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient25",
+        name: "swordfish",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient26",
+        name: "cod",
+        catagories: ["catagory3"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient27",
+        name: "banana",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient28",
+        name: "cherry",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient29",
+        name: "raspberry",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient30",
+        name: "apple",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient31",
+        name: "apricot",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient32",
+        name: "kiwi",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient33",
+        name: "orange",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient34",
+        name: "blueberry",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient35",
+        name: "blackberry",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient36",
+        name: "grapefruit",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient37",
+        name: "honeydew melon",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient38",
+        name: "cantoulope",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient39",
+        name: "grapes",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient40",
+        name: "plum",
+        catagories: ["catagory4"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient42",
+        name: "pepper",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient43",
+        name: "salt",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient44",
+        name: "oregano",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient45",
+        name: "thyme",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient46",
+        name: "basil",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient47",
+        name: "nutmeg",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient48",
+        name: "allspice",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient49",
+        name: "cinnamon",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient50",
+        name: "ginger",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient51",
+        name: "bay leaf",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient52",
+        name: "marjoram",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient53",
+        name: "tumeric",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient54",
+        name: "sage",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient55",
+        name: "curry",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient56",
+        name: "vanilla",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient57",
+        name: "cumin",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient58",
+        name: "carraway",
+        catagories: ["catagory5"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient59",
+        name: "chili flakes",
+        catagories: ["catagory5", "catagory6"],
+        relativeValues: [],
+      },
+      {
+        id: "ingredient60",
+        name: "red chili",
+        catagories: ["catagory4", "catagory5", "catagory6"],
+        relativeValues: [],
+      },
     ],
   };
 })();
 
 const searchIngredients = (query, breadcrumbs = []) => {
-  const dataSet = traversePath(data, breadcrumbs);
+  const lastestBreadCrumb =
+    breadcrumbs && breadcrumbs[breadcrumbs.length - 1]?.id;
+  const dataSet = lastestBreadCrumb
+    ? data.ingredients.filter(({ catagories }) =>
+        catagories.includes(lastestBreadCrumb)
+      )
+    : data.ingredients;
   const searchOptions = {
     output: {
       ingredientsThatStartWithQuery: {
         type: "string",
         flags: "iy",
+        within: "name",
       },
       ingredientsThatContainQuery: {
         type: "string",
         flags: "i",
-      },
-      catagories: {
-        type: "array",
-        flags: "iy",
+        within: "name",
       },
     },
     recursive: true,
   };
   const matches = search(query, dataSet, searchOptions);
-  const formatResult = ({ key, value, path, type }) => ({
-    title: !Number.isInteger(key) ? key : value,
-    path: [...breadcrumbs, ...path],
-    isCatagory: type === "catagory",
-  });
   const matchSet = new Set(
     Object.keys(matches)
       .map((container) => {
-        const type = container === "catagories" ? "catagory" : "ingredient";
         return [
-          ...matches[container].map((result) =>
-            JSON.stringify(formatResult({ ...result, type }))
+          ...matches[container].map(({ value, path }) =>
+            JSON.stringify({
+              title: value,
+              path,
+            })
           ),
         ];
       })
@@ -100,12 +430,13 @@ const searchIngredients = (query, breadcrumbs = []) => {
   );
   return [...matchSet].map((json) => JSON.parse(json));
 };
-const getCatagories = (path = []) => {
-  const dataSet = traversePath(data, path);
-  return Object.keys(dataSet).map((item) => ({
-    title: item,
-    isCatagory: true,
-  }));
+
+const getCatagories = (breadcrumbs) => {
+  const lastestBreadCrumb = breadcrumbs && breadcrumbs[breadcrumbs.length - 1];
+  const dataSet = lastestBreadCrumb
+    ? data.catagories.filter(({ parent }) => parent === lastestBreadCrumb.id)
+    : data.catagories.filter(({ parent }) => !parent);
+  return Object.values(dataSet).map(({ name, id }) => ({ title: name, id }));
 };
 
 export { searchIngredients, getCatagories };
