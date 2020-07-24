ff3f966b449b9703ae8ac0c2131e1cb3cf428a4a
jidemobell <jidd32@gmail.com>
2020-07-24 20:38:38 +0100

    update db index module

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,16 @@
 const User = require("../models/index");
+const util = require("util");
+
+const Index = require("../models/index");
 
 
 
+const craeteIndex = async() =>{
+	console.log("creating db index==")
+	try {
+		
+	} catch (error) {
+		
+	}
+}
 
