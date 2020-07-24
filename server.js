f547a4d4a7a33ff1c9de936cb9735669f703ffb6
jidemobell <jidd32@gmail.com>
2020-07-24 20:16:15 +0100

    fix server.js bug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use('/auth', require('./server/routes/auth'));
 app.use('/recipe', require('./server/routes/recipe'))
 app.use('/user', require('./server/routes/user'))
-app.user('/index', require('./server/routes/index'))
+app.use('/index', require('./server/routes/index'))
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
