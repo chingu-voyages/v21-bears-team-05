71367c1f5ff3580c5621c85137507bb7d6b6a1ca
lily-law <lilylaw.dev@gmail.com>
2020-07-24 12:16:13 +0100

    Replace title with name

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './ListItem.css'
 
-const ListItem = ({ title, removeSelf, onClick, type = ''}) => (<>
+const ListItem = ({ name, removeSelf, onClick, type = ''}) => (<>
     {onClick ? 
         <button className={`item item--${type}`} onClick={onClick}>
-            <p className="item__title">{title}</p>
+            <p className="item__name">{name}</p>
         </button> :
         <div className={`item item--${type}`}>
-            <p className="item__title">{title}</p>
+            <p className="item__name">{name}</p>
             {removeSelf && <button className="item__remove" onClick={removeSelf}>X</button>}
         </div> 
     }
