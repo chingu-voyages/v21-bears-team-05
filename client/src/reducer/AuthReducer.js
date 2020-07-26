6ad043244af4c6c915bc986658c1b0cfdfe71097
Laptop <pierrolabo@gmail.com>
2020-07-26 23:28:00 +0200

    Remove sensitive data

--- client/src/reducer/AuthReducer.js
+++ client/src/reducer/AuthReducer.js
@@ -1,50 +1,41 @@
 const AuthReducer = (state, action) => {
   switch (action.type) {
-    case 'LOGIN':
+    case "LOGIN":
       const userPayload = action.payload.user;
       const token = action.payload.token;
 
       //  Data structure for OAUTH and local will be different
       let user = {};
       switch (userPayload.method) {
-        case 'facebook':
+        case "facebook":
           user = {
             //  We use the database _id not the OAUTH _id
             id: userPayload.id,
-            //email: userPayload.facebook.email,
-            //name: userPayload.facebook.name,
-            //surname: userPayload.facebook.surname,
           };
           break;
-        case 'google':
+        case "google":
           user = {
             //  We use the database _id not the OAUTH _id
             id: userPayload.id,
-            //email: userPayload.google.email,
-            //name: userPayload.google.name,
-            //surname: userPayload.google.surname,
           };
           break;
-        case 'local':
+        case "local":
           user = {
             id: userPayload.id,
-            //email: userPayload.local.email,
-            //name: userPayload.local.name,
-            //surname: userPayload.local.surname,
           };
           break;
         default:
           break;
       }
-      localStorage.setItem('user', JSON.stringify(user));
-      localStorage.setItem('token', JSON.stringify(token));
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("token", JSON.stringify(token));
       return {
         ...state,
         isAuthenticated: true,
         user: user,
         token: token,
       };
-    case 'LOGOUT':
+    case "LOGOUT":
       localStorage.clear();
       return {
         ...state,
