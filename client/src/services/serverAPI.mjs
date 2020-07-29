9fe2cf6d04cee4db9bd04f70f969bc973184d13d
Laptop <pierrolabo@gmail.com>
2020-07-29 14:54:34 +0200

    Add Observer

--- client/src/services/serverAPI.mjs
+++ client/src/services/serverAPI.mjs
@@ -1,13 +1,40 @@
 import axios from "axios";
 
+/*
+  Observer pattern
+  We use this function as a data tunnel between localDB and our app
+  import ObserverServerAPI then subscribe your callback
+  it will be called by fire()
+*/
+function ObserverServerAPI() {
+  this.handlers = [];
+}
+
+ObserverServerAPI.prototype = {
+  subscribe: function (fn) {
+    this.handlers.push(fn);
+  },
+  fire: function (type, data, thisObj) {
+    var scope = thisObj || window;
+    this.handlers.forEach(function (item) {
+      item.call(scope, type, data);
+    });
+  },
+};
+
+export const observerServerAPI = new ObserverServerAPI();
+
 //  Check if Backend is online
 const isOnline = async () => {
   const res = await axios.get("http://localhost:5000/isOnline");
+  //observerServerAPI.fire("IS_ONLINE", 200 ? true : null);
   return res.status === 200 ? true : null;
 };
 
 const getData = async ({ destination, ref }) => {
   console.log("API CALL", destination);
+  //  Send a loading message
+  //  Will be dispatched in our serverAPI reducer
   const token = JSON.parse(localStorage.getItem("token"));
   const config = {
     headers: {
@@ -24,19 +51,27 @@ const getData = async ({ destination, ref }) => {
       config
     );
   } else {
-    //TODO
-    res = axios.get(`http://localhost:5000/${destination}/`, config);
+    //  Can throw an error if we don't get data back
+    //  Right now it doesn't break our logic because it return a promise
+    //  TODO
+    //res = axios.get(`http://localhost:5000/${destination}/`, config);
   }
   if (res) {
+    observerServerAPI.fire("GET_DATA", res);
     return res.data;
+  } else {
+    //observerServerAPI.fire("GET_DATA", res);
   }
 };
 const postData = async ({ destination, data }) => {
+  //  Send a loading message
+  //  Will be dispatched in our serverAPI reducer
   console.log(
     `postData method; destination: ${destination}, data: ${JSON.stringify(
       data
     )}`
   );
+  observerServerAPI.fire("LOADING");
   const token = JSON.parse(localStorage.getItem("token"));
   const config = {
     headers: {
@@ -45,12 +80,17 @@ const postData = async ({ destination, data }) => {
     },
   };
   const body = JSON.stringify(data);
-
-  const res = await axios.post(
-    `http://127.0.0.1:5000/${destination}/${data.id}`,
-    body,
-    config
-  );
+  let res = null;
+  try {
+    res = await axios.post(
+      `http://127.0.0.1:5000/${destination}/${data.id}`,
+      body,
+      config
+    );
+  } catch (error) {
+    const errorCode = error.response.status;
+    observerServerAPI.fire("POST_DATA", { status: errorCode });
+  }
   return res;
 };
 const putData = async ({ destination, ref, data }) => {
