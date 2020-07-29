ed5f16ca15872ee3161e74a92a2b492e6a1f55a4
jidemobell <jidd32@gmail.com>
2020-07-29 16:25:28 +0100

    update recipe model

--- db/index.js
+++ db/index.js
@@ -1,36 +1,33 @@
-require('dotenv').config();
-const mongoose = require("mongoose")
-const helper = require('../lib/db')
+require("dotenv").config();
+const mongoose = require("mongoose");
+const helper = require("../lib/db");
 
+const mongoURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : process.env.NODE_ENV === "test"
+    ? "mongodb://localhost:27017/feedmetest"
+    : process.env.MONGO_URI_DEV;
 
-const mongoURI = process.env.NODE_ENV === "production"
-	? process.env.MONGO_URI
-	: process.env.NODE_ENV === "test"
-	?  "mongodb://localhost:27017/feedmetest"
-	: process.env.MONGO_URI_DEV
-
-
-	//create connection
+//create connection
 mongoose
-.connect(mongoURI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	// autoIndex: false,
-	useCreateIndex: true,
-	bufferCommands: false,
-	keepAlive: true,
-	keepAliveInitialDelay: 300000,
-})
-.catch((err) => helper.handleDbConnectionError(err));
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    bufferCommands: false,
+    keepAlive: true,
+  })
+  .catch((err) => helper.handleDbConnectionError(err));
 
-if(process.env.NODE_ENV === "development") mongoose.set("debug", true)
+if (process.env.NODE_ENV === "development") mongoose.set("debug", true);
 
 //check for connection success or failure
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => console.log('db connected!'))
-db.on('disconnected', () => console.log('db disconnected!'))
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("db connected!"));
+db.on("disconnected", () => console.log("db disconnected!"));
 
 process.on("SIGINT", () => {
   console.log("Mongoose disconnected on exit process");
