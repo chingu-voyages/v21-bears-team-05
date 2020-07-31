0bc79500089cdf554f7284c5ae351629e9aa48c3
Laptop <pierrolabo@gmail.com>
2020-07-31 15:11:14 +0200

    Refactored Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,42 +1,53 @@
-import React, { useState } from 'react';
-import './Login.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Login.css";
 
-import { NavLink } from 'react-router-dom';
-
-const Login = ({ errorMessage, handleLogin }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ onLoginSubmit, error }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmit = (e) => {
     e.preventDefault();
-    handleLogin({ email, password });
+    onLoginSubmit({ email, password });
+  };
+
+  //  If there was an Error in the precedent request
+  //  Show a personalised error message
+  const parseError = (error) => {
+    const errorCode = error.response.status;
+    switch (errorCode) {
+      case 401:
+        return "Email not found!";
+      default:
+        break;
+    }
   };
   return (
-    <div className='login-container'>
+    <div className="login-local-container">
       <h2>Log in into your account</h2>
-      <form className='login-container__form'>
-        {errorMessage ? <h3>Error login, try again</h3> : ''}
+      {error ? <h3>Error Login {parseError(error)}</h3> : ""}
+      <form className="login-local-container__form">
         <label>
           Email
           <input
-            placeholder='Your email address'
-            type='email'
+            placeholder="Your email address"
+            type="email"
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label>
           Password
           <input
-            placeholder='Your password'
-            type='password'
+            placeholder="Your password"
+            type="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <NavLink to='/forgotPassword'>Forgot your password ?</NavLink>
         </label>
-        <div className='login-container__form__options'>
+        <div className="login-container__form__options">
+          <NavLink to="/forgotPassword">Forgot your password ?</NavLink>
           <button
             onClick={onSubmit}
-            className='login-container__form__options-submit'
+            className="login-login-container__form__options-submit"
           >
             Submit
           </button>
