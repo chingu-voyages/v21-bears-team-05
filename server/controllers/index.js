48cd8af64d11359496594ceac7e7210b3683635b
lily-law <lilylaw.dev@gmail.com>
2020-08-06 14:34:59 +0100

    Update index controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,39 +1,28 @@
 const Index = require("../models/index");
 const queryHelper = require("../../lib/query");
 
-const createIndex = async (res, next) => {
-  const allTopRecipes = await queryHelper.listTopTenRecipes();
-
-  try {
-    const index = await Index.create({
-      topRecipes: [
-        ...allTopRecipes.map((recipe) => {
-          return {
-            recipeRef: recipe.id,
-            ingredients: recipe.ingredients,
-            tags: recipe.tags,
-          };
-        }),
-      ],
-    });
-
-    res.status(200).send({ appDbIndex: index });
-  } catch (error) {
-    next(error);
-  }
-};
-
 const getIndex = async (res, next) => {
   console.log("getting db index==");
   try {
     const index = await Index.find({});
-    res.status(200).send({ appDbIndex: index });
+    const recipesSortedByRating = await queryHelper.sortRecipesByRating();
+    res.status(200).send({ data: {
+        ...index,
+        recipes: [
+          ...recipesSortedByRating.map((recipe) => {
+            return {
+              recipeRef: recipe.id,
+              ingredients: recipe.ingredients,
+              tags: recipe.tags,
+            };
+          }),
+        ]
+    }});
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-  createIndex,
-  getIndex,
+  getIndex
 };
