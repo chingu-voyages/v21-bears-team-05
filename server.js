7a2edff5df222637b4c6bfbc8ec090d92220ca29
Lily <lily@pop-os.localdomain>
2020-08-09 12:30:42 +0100

    Fix typos in server production route

--- server.js
+++ server.js
@@ -70,9 +70,9 @@ app.use((error, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join((__dirname = "client/build/index.html")));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 } else {
   app.get("*", (req, res) => {
