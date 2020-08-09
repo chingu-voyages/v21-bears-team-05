679210ad52b206be0f701e4ea9778d08955355e3
Lily <lily@pop-os.localdomain>
2020-08-09 12:35:48 +0100

    Remove server 404 handler

--- server.js
+++ server.js
@@ -59,11 +59,6 @@ app.get("/api/", function (req, res) {
   });
 });
 
-//404 handler
-app.use(function (req, res, next) {
-  res.status(404).send("oop's, can't find that!");
-});
-
 // error handler middleware
 app.use((error, req, res, next) => {
   ErrorHandler.handleError(error, req, res);
